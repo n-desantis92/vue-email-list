5f1d85105689b8b1438d3cf3e7e1c149f0a0523a
n-desantis92 <dsnwebdev@gmail.com>
2021-04-07 15:43:28 +0200

    spostato ciclo for

--- script/script.js
+++ script/script.js
@@ -7,18 +7,16 @@ var app = new Vue ({
 
   mounted: function () {
 
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    for (var i = 0; i < this.listaMail.length < 10; i++) {
+
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
       .then( (response) => {
         const result = response.data;
         let email = result.response;
-        console.log(email);
-        console.log(this.listaMail.length);
-        for (var i = 0; this.listaMail.length < 10; i++) {
-          if (!(this.listaMail.includes(email))) {
-            this.listaMail.push(email);
-          };
-          console.log(this.listaMail);
-        };
+
+        this.listaMail.push(email);
+        console.log(this.listaMail);
       });
+    };
   }
 })
